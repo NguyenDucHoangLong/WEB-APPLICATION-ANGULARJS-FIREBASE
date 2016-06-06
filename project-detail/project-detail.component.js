1caa0535ef4563836ce2bc88cdf3287cfe7bb9d6
trungnguyen281 <ndtrunga195@gmail.com>
2016-06-07 01:58:52 +0700

    Update
    Thay đổi lại cấu trúc thư mục.
    Sử dụng component
    Load dữ liệu (1 phần)

---
+++ project-detail/project-detail.component.js
@@ -0,0 +1,15 @@
+angular.
+ 	module('myApp').
+  	component('projectDetail', {
+    	templateUrl: 'project-detail/project-detail.template.html',
+    	controller: ['$http', '$routeParams',
+	      	function ProjectDetailController($http, $routeParams) {
+	        	var self = this;
+
+	        	$http.get('database/project.json').then(function(response) {
+	        	self.id = $routeParams.projectId;
+	          	self.project = response.data[self.id - 1];
+	       	 	});
+	        }
+	    ]
+  	});
---
+++ project-list/project-list.component.js
@@ -0,0 +1,14 @@
+angular.
+	module('myApp').
+	component('projectList', {
+		templateUrl: 'project-list/project-list.template.html',
+		controller: ['$http',
+	      	function ProjectListController($http) {
+	        	var self = this;
+
+	        	$http.get('database/project.json').then(function(response) {
+	          	self.projects = response.data;
+	       	 	});
+	        }
+	    ]
+	});
